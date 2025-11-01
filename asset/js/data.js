a4f1247b9199903bc18d88b4209c100749fb79c4
JungHyun-Ahn <84699689+JungHyun-Ahn@users.noreply.github.com>
2025-11-02 00:41:01 +0900

    Update data.js

--- asset/js/data.js
+++ asset/js/data.js
@@ -7,13 +7,13 @@ const designData = [
     name: "서울1인가구지원센터",
     type: "카드뉴스",
     detail: "삼행시 공모전 발표 카드뉴스 시안 제작",
-    thumImg: "./asset/images/works/design/item1_thum.png",
+    thumImg: "./asset/images/works/design/item1_thum.jpg",
     link: "https://www.instagram.com/p/CX-actwLJVb/",
     images: [
-      "./asset/images/works/design/item1_img(1).png",
-      "./asset/images/works/design/item1_img(2).png",
-      "./asset/images/works/design/item1_img(3).png",
-      "./asset/images/works/design/item1_img(4).png"
+      "./asset/images/works/design/item1_img(1).jpg",
+      "./asset/images/works/design/item1_img(2).jpg",
+      "./asset/images/works/design/item1_img(3).jpg",
+      "./asset/images/works/design/item1_img(4).jpg"
     ]
   },
   {
@@ -22,10 +22,10 @@ const designData = [
     name: "서울1인가구지원센터",
     type: "카드뉴스",
     detail: "연말 카드뉴스 시안 제작",
-    thumImg: "./asset/images/works/design/item2_thum.png",
+    thumImg: "./asset/images/works/design/item2_thum.jpg",
     link: "https://www.instagram.com/p/CmaRSgTv7VR/",
     images: [
-      "./asset/images/works/design/item2_img.png"
+      "./asset/images/works/design/item2_img.jpg"
     ]
   },
   {
@@ -34,13 +34,13 @@ const designData = [
     name: "서울1인가구지원센터",
     type: "카드뉴스",
     detail: "이벤트 카드뉴스 시안 제작",
-    thumImg: "./asset/images/works/design/item3_thum.png",
+    thumImg: "./asset/images/works/design/item3_thum.jpg",
     link: "https://www.instagram.com/p/Cek1V0ql7DR/",
     images: [
-      "./asset/images/works/design/item3_img(1).png",
-      "./asset/images/works/design/item3_img(2).png",
-      "./asset/images/works/design/item3_img(3).png",
-      "./asset/images/works/design/item3_img(4).png"
+      "./asset/images/works/design/item3_img(1).jpg",
+      "./asset/images/works/design/item3_img(2).jpg",
+      "./asset/images/works/design/item3_img(3).jpg",
+      "./asset/images/works/design/item3_img(4).jpg"
     ]
   },
   {
@@ -49,10 +49,10 @@ const designData = [
     name: "서울1인가구지원센터",
     type: "ETC(포스터)",
     detail: "카드뉴스 포스터 리사이즈",
-    thumImg: "./asset/images/works/design/item4_thum.png",
+    thumImg: "./asset/images/works/design/item4_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item4_img.png"
+      "../asset/images/works/design/item4_img.jpg"
     ]
   },
   {
@@ -61,11 +61,11 @@ const designData = [
     name: "서울1인가구지원센터",
     type: "ETC(네임카드)",
     detail: "공식 블로그 네임카드 시안 제작",
-    thumImg: "./asset/images/works/design/item5_thum.png",
+    thumImg: "./asset/images/works/design/item5_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item5_img(1).png",
-      "../asset/images/works/design/item5_img(2).png"
+      "../asset/images/works/design/item5_img(1).jpg",
+      "../asset/images/works/design/item5_img(2).jpg"
     ]
   },
   {
@@ -74,10 +74,10 @@ const designData = [
     name: "광주실감콘텐츠큐브(GCC)",
     type: "홈페이지 디자인",
     detail: "홈페이지 시안 제작",
-    thumImg: "./asset/images/works/design/item6_thum.png",
+    thumImg: "./asset/images/works/design/item6_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item6_img.png"
+      "../asset/images/works/design/item6_img.jpg"
     ]
   },
   {
@@ -86,10 +86,10 @@ const designData = [
     name: "진명스포아트",
     type: "카드뉴스",
     detail: "이벤트 카드뉴스 시안 제작",
-    thumImg: "./asset/images/works/design/item7_thum.png",
-    link: "https://www.instagram.com/p/Cf20-R8LjBi/",
+    thumImg: "./asset/images/works/design/item7_thum.jpg",
+    link: "",
     images: [
-      "../asset/images/works/design/item7_img.png"
+      "../asset/images/works/design/item7_img.jpg"
     ]
   },
   {
@@ -98,10 +98,10 @@ const designData = [
     name: "진명스포아트",
     type: "카드뉴스",
     detail: "이벤트 카드뉴스 시안 제작",
-    thumImg: "./asset/images/works/design/item8_thum.png",
+    thumImg: "./asset/images/works/design/item8_thum.jpg",
     link: "https://www.instagram.com/p/CibeQryJFbX/",
     images: [
-      "../asset/images/works/design/item8_img.png"
+      "../asset/images/works/design/item8_img.jpg"
     ]
   },
   {
@@ -110,7 +110,7 @@ const designData = [
     name: "비비런",
     type: "ETC(프로그램북)",
     detail: "비비런 프로그램북 제작",
-    thumImg: "./asset/images/works/design/item9_thum.png",
+    thumImg: "./asset/images/works/design/item9_thum.jpg",
     link: "",
     images: [
       "../asset/images/works/design/item9_img(1).jpg",
@@ -123,12 +123,12 @@ const designData = [
     name: "MU:CON 2022",
     type: "카드뉴스",
     detail: "이벤트 카드뉴스 시안 제작",
-    thumImg: "./asset/images/works/design/item10_thum.png",
+    thumImg: "./asset/images/works/design/item10_thum.jpg",
     link: "https://www.instagram.com/p/CkFG0MxPsfN/",
     images: [
-      "../asset/images/works/design/item10_img(1).png",
-      "../asset/images/works/design/item10_img(2).png",
-      "../asset/images/works/design/item10_img(3).png"
+      "../asset/images/works/design/item10_img(1).jpg",
+      "../asset/images/works/design/item10_img(2).jpg",
+      "../asset/images/works/design/item10_img(3).jpg"
     ]
   },
   {
@@ -137,10 +137,10 @@ const designData = [
     name: "MU:CON 2022",
     type: "카드뉴스",
     detail: "이벤트 카드뉴스 시안 제작",
-    thumImg: "./asset/images/works/design/item11_thum.png",
+    thumImg: "./asset/images/works/design/item11_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item11_img.png"
+      "../asset/images/works/design/item11_img.jpg"
     ]
   },
   {
@@ -149,11 +149,11 @@ const designData = [
     name: "MU:CON 2022",
     type: "ETC(프로그램북)",
     detail: "MU:CON 2022 프로그램북 시안 제작",
-    thumImg: "./asset/images/works/design/item12_thum.png",
+    thumImg: "./asset/images/works/design/item12_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item12_img(1).png",
-      "../asset/images/works/design/item12_img(2).png"
+      "../asset/images/works/design/item12_img(1).jpg",
+      "../asset/images/works/design/item12_img(2).jpg"
     ]
   },
   {
@@ -162,11 +162,11 @@ const designData = [
     name: "MU:CON 2022",
     type: "홈페이지 디자인",
     detail: "홈페이지 시안 제작",
-    thumImg: "./asset/images/works/design/item13_thum.png",
+    thumImg: "./asset/images/works/design/item13_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item13_img(1).png",
-      "../asset/images/works/design/item13_img(2).png"
+      "../asset/images/works/design/item13_img(1).jpg",
+      "../asset/images/works/design/item13_img(2).jpg"
     ]
   },
   {
@@ -175,11 +175,11 @@ const designData = [
     name: "mbc아카데미 원격평생교육원",
     type: "카드뉴스",
     detail: "홍보 카드뉴스 시안 제작",
-    thumImg: "./asset/images/works/design/item14_thum.png",
+    thumImg: "./asset/images/works/design/item14_thum.jpg",
     link: "https://www.facebook.com/mbcacademy/posts/243083071367220",
     images: [
-      "../asset/images/works/design/item14_img(1).png",
-      "../asset/images/works/design/item14_img(2).png",
+      "../asset/images/works/design/item14_img(1).jpg",
+      "../asset/images/works/design/item14_img(2).jpg",
     ]
   },
   {
@@ -188,15 +188,15 @@ const designData = [
     name: "헤이스쿨스 클럽 (남이섬)",
     type: "카드뉴스",
     detail: "홍보 카드뉴스 시안 제작",
-    thumImg: "../asset/images/works/design/item15_thum.png",
+    thumImg: "../asset/images/works/design/item15_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item15_img(1).png",
-      "../asset/images/works/design/item15_img(2).png",
-      "../asset/images/works/design/item15_img(3).png",
-      "../asset/images/works/design/item15_img(4).png",
-      "../asset/images/works/design/item15_img(5).png",
-      "../asset/images/works/design/item15_img(6).png"
+      "../asset/images/works/design/item15_img(1).jpg",
+      "../asset/images/works/design/item15_img(2).jpg",
+      "../asset/images/works/design/item15_img(3).jpg",
+      "../asset/images/works/design/item15_img(4).jpg",
+      "../asset/images/works/design/item15_img(5).jpg",
+      "../asset/images/works/design/item15_img(6).jpg"
     ]
   },
   {
@@ -205,10 +205,10 @@ const designData = [
     name: "해외문화홍보원",
     type: "카드뉴스",
     detail: "이벤트 카드뉴스 시안 제작",
-    thumImg: "../asset/images/works/design/item16_thum.png",
+    thumImg: "../asset/images/works/design/item16_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item16_img.png"
+      "../asset/images/works/design/item16_img.jpg"
     ]
   },
   {
@@ -217,10 +217,10 @@ const designData = [
     name: "해외문화홍보원",
     type: "카드뉴스",
     detail: "이벤트 카드뉴스 시안 제작",
-    thumImg: "../asset/images/works/design/item17_thum.png",
+    thumImg: "../asset/images/works/design/item17_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item17_img.png"
+      "../asset/images/works/design/item17_img.jpg"
     ]
   },
   {
@@ -229,11 +229,11 @@ const designData = [
     name: "해외문화홍보원",
     type: "홈페이지 디자인",
     detail: "홈페이지 시안 제작",
-    thumImg: "../asset/images/works/design/item18_thum.png",
+    thumImg: "../asset/images/works/design/item18_thum.jpg",
     link: "",
     images: [
-      "../asset/images/works/design/item18_img(1).png",
-      "../asset/images/works/design/item18_img(2).png"
+      "../asset/images/works/design/item18_img(1).jpg",
+      "../asset/images/works/design/item18_img(2).jpg"
     ]
   },
   {
